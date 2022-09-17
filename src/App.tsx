428e714258e6f12cdfa214fc0a88d18d8869a87c
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-09-17 23:05:59 +0300

    start from a blank page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-import { Component, createSignal } from 'solid-js';
-import './App.scss';
-import Counter from './Counter';
+import { Component } from "solid-js";
 
-const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 1);
-
-  return (
-    <>
-      <div>
-        <h1 class="header">{counter()}</h1>
-      </div>
-      <Counter />
-    </>
-  );
-};
-
-export default App;
+export const App: Component = () => <div>hi</div>;
