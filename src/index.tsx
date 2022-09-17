26947c95b1781217364cc01d763f99934a0c945f
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-09-17 23:49:35 +0300

    add css vars

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { render } from "solid-js/web";
 import { App } from "./App";
 
 import "./styles/reset.css";
+import "./styles/vars.scss";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
