31dce0d0a974f910876ddb23dec1087fb5a906b5
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-09-17 23:52:40 +0300

    refactor reset.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import { render } from "solid-js/web";
 import { App } from "./App";
 
-import "./styles/reset.css";
+import "./styles/reset.scss";
 import "./styles/vars.scss";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
