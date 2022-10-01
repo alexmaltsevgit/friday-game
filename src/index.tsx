09e48b8db3f11bc2e7ba3f65342ed8dfe92d5ef5
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-10-01 23:09:12 +0300

    add router wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
-/* @refresh reload */
+import { Router } from "@solidjs/router";
 import { render } from "solid-js/web";
+
 import { App } from "./App";
 
 import "./styles/reset.scss";
 import "./styles/vars.scss";
 import "./styles/default.scss";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement
+);
