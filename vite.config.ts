9bb31364f21ac583dfaf97a88f23f33cb97f8d18
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-10-01 23:07:41 +0300

    change dev port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import solidPlugin from "vite-plugin-solid";
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
-    port: 3000,
+    port: 3050,
   },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
