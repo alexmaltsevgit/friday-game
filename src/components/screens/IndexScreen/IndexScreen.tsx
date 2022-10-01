c5c9ac7e455df2794f1fad22d950e93bae8c852e
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-10-01 23:13:30 +0300

    add dummy socket usage

--- src/components/screens/IndexScreen/IndexScreen.tsx
+++ src/components/screens/IndexScreen/IndexScreen.tsx
@@ -2,6 +2,8 @@ import { createSignal } from "solid-js";
 
 import { Button } from "@/components/shared";
 import { model as modelDir } from "@/directives";
+import { socket } from "@/socket";
+import { SocketRoomEvent } from "@/types";
 
 import decor from "@/assets/index-screen-decor.png";
 import styles from "./IndexScreen.module.scss";
@@ -10,6 +12,8 @@ const model = modelDir;
 
 export const IndexScreen = () => {
   const [name, setName] = createSignal();
+
+  const createRoom = () => socket.emit(SocketRoomEvent.CreateRoom);
 
   return (
     <div class={styles.root}>
@@ -21,7 +25,7 @@ export const IndexScreen = () => {
             <input use:model={[name, setName]} />
 
             <div class={styles.buttons}>
-              <Button>Создать игру</Button>
+              <Button onClick={createRoom}>Создать игру</Button>
 
               <Button>Присоединиться к игре</Button>
             </div>
