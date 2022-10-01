40eccb07f3cf3a6fdf66dcef8b2ae5451de33706
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-10-01 23:12:08 +0300

    add index route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Component } from "solid-js";
+import { Routes, Route } from "@solidjs/router";
 
 import { IndexScreen } from "@/components/screens";
 
-export const App: Component = () => <IndexScreen />;
+export const App: Component = () => (
+  <Routes>
+    <Route path={"/"} component={IndexScreen} />
+  </Routes>
+);
