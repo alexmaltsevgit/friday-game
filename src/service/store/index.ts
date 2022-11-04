425ccdcf84a4b7d4912bf7154b6e8c4240ae2d18
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-04 21:41:22 +0300

    add store

---
+++ src/service/store/index.ts
@@ -0,0 +1,41 @@
+import { Client } from "colyseus.js";
+import { createStore } from "solid-js/store";
+import { createMutation } from "@tanstack/solid-query";
+import { MapSchema } from "@colyseus/schema";
+
+const socket = new Client(import.meta.env.VITE_WS_SERVER_URL_DEV);
+
+type Player = {
+  realName: string;
+  fictionName: string;
+};
+
+type GameState = {
+  ownerId?: string;
+  players: MapSchema<Player>;
+};
+
+type RoomState = {
+  roomId?: string;
+  myId?: string;
+  game?: GameState;
+};
+
+const [roomStore, setRoomStore] = createStore<RoomState | null>(null);
+
+export const useCreateRoom = () => {
+  const mutationFn = async (realName: string) => {
+    const room = await socket.create<GameState>("game", { realName });
+
+    setRoomStore("roomId", room.id);
+    setRoomStore("myId", room.sessionId);
+
+    room.onStateChange((s) => setRoomStore("game", s));
+
+    return room;
+  };
+
+  return createMutation(mutationFn);
+};
+
+export const useRoomState = () => roomStore;
--- src/socket/index.ts
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-export const socket = io(import.meta.env.VITE_SERVER_URL_DEV);
-
-socket.on("connect", () => {
-  console.log("socket connect");
-});
