73658660f5cc2922f01b4da5591d11c2d677d993
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-05 19:01:05 +0300

    add mapColyseusStateToPlainState

---
+++ src/utils/mapColyseusStateToPlainState.ts
@@ -0,0 +1,52 @@
+import { ArraySchema, MapSchema } from "@colyseus/schema";
+
+import {
+  ColyseusPrimitive,
+  ColyseusState,
+  PlainStateFromColyseusState,
+} from "@/types";
+
+const isColyseusPrimitive = (value: unknown): value is ColyseusPrimitive =>
+  typeof value === "string" ||
+  typeof value === "number" ||
+  typeof value === "boolean" ||
+  typeof value === "undefined" ||
+  value === null;
+
+const isMapSchema = (value: unknown): value is MapSchema =>
+  typeof value === "object" && value !== null && "get" in value;
+
+const isArraySchema = (value: unknown): value is ArraySchema =>
+  typeof value === "object" && value !== null && "splice" in value;
+
+const plainArrayFromArraySchema = <T>(value: ArraySchema<T>) => value.toArray();
+
+const plainObjectFromMapSchema = <T>(value: MapSchema<T>) => value.toJSON();
+
+export const mapColyseusStateToPlainState = <T extends ColyseusState>(
+  colyseusState: T
+) => {
+  const resultingPlainState = {};
+
+  Object.entries(colyseusState).forEach(([key, value]) => {
+    if (isColyseusPrimitive(value)) {
+      resultingPlainState[key] = value;
+      return;
+    }
+
+    if (isMapSchema(value)) {
+      resultingPlainState[key] = plainObjectFromMapSchema(value);
+      return;
+    }
+
+    if (isArraySchema(value)) {
+      resultingPlainState[key] = plainArrayFromArraySchema(value);
+      return;
+    }
+
+    // assume it is nested object
+    resultingPlainState[key] = mapColyseusStateToPlainState(value);
+  });
+
+  return resultingPlainState as PlainStateFromColyseusState<T>;
+};
