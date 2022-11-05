f1149fc5521c01c46435e6da107f3e9197124695
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-05 19:00:07 +0300

    add type PlainStateFromColyseusState

---
+++ src/types/colyseus-adapter.ts
@@ -0,0 +1,27 @@
+import { ArraySchema, MapSchema } from "@colyseus/schema";
+
+type PlainObjectFromMapSchema<T> = T extends MapSchema<infer R>
+  ? Record<string, R>
+  : never;
+
+type PlainArrayFromArraySchema<T> = T extends ArraySchema<infer R>
+  ? R[]
+  : never;
+
+export type ColyseusPrimitive = undefined | null | number | string | boolean;
+
+export type ColyseusState = {
+  [Key: string]: MapSchema | ArraySchema | ColyseusPrimitive | ColyseusState;
+};
+
+export type PlainStateFromColyseusState<T extends ColyseusState> = {
+  [Key in keyof T]: T[Key] extends MapSchema
+    ? PlainObjectFromMapSchema<T[Key]>
+    : T[Key] extends ArraySchema
+    ? PlainArrayFromArraySchema<T[Key]>
+    : T[Key] extends ColyseusState
+    ? PlainStateFromColyseusState<T[Key]>
+    : T[Key] extends ColyseusPrimitive
+    ? T[Key]
+    : never;
+};
