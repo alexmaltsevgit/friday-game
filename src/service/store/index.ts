5247ca1bdaf9c60954ad0b67c7b6641e9906508c
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-05 19:01:33 +0300

    fix store reactivity

--- src/service/store/index.ts
+++ src/service/store/index.ts
@@ -1,41 +1,52 @@
 import { Client } from "colyseus.js";
 import { createStore } from "solid-js/store";
 import { createMutation } from "@tanstack/solid-query";
-import { MapSchema } from "@colyseus/schema";
 
-import { RoomStatus } from "@/types";
+import {
+  GameColyseusState,
+  PlainStateFromColyseusState,
+  RoomColyseusState,
+} from "@/types";
+import { mapColyseusStateToPlainState } from "@/utils";
 
 const socket = new Client(import.meta.env.VITE_WS_SERVER_URL_DEV);
 
-type Player = {
-  realName: string;
-  fictionName: string;
-};
-
-type GameState = {
-  ownerId?: string;
-  status: RoomStatus;
-  players: MapSchema<Player>;
-};
-
-type RoomState = {
-  roomId?: string;
-  myId?: string;
-  game?: GameState;
-};
-
-const [roomStore, setRoomStore] = createStore<RoomState | null>(null);
+const [roomStore, setRoomStore] =
+  createStore<PlainStateFromColyseusState<RoomColyseusState> | null>(null);
 
 export const useCreateRoom = () => {
   const mutationFn = async (realName: string) => {
-    const room = await socket.create<GameState>("game", { realName });
+    const room = await socket.create<GameColyseusState>("game", { realName });
 
     room.onStateChange((s) => {
       // don't move this few lines out of 'onStateChange'
       setRoomStore("roomId", room.id);
       setRoomStore("myId", room.sessionId);
 
-      setRoomStore("game", s);
+      setRoomStore("game", mapColyseusStateToPlainState(s));
+    });
+
+    return room;
+  };
+
+  return createMutation(mutationFn);
+};
+
+type JoinRoomPayload = {
+  realName: string;
+  code: string;
+};
+
+export const useJoinRoom = () => {
+  const mutationFn = async ({ realName, code }: JoinRoomPayload) => {
+    const room = await socket.joinById<GameColyseusState>(code, { realName });
+
+    room.onStateChange((s) => {
+      // don't move this few lines out of 'onStateChange'
+      setRoomStore("roomId", room.id);
+      setRoomStore("myId", room.sessionId);
+
+      setRoomStore("game", mapColyseusStateToPlainState(s));
     });
 
     return room;
