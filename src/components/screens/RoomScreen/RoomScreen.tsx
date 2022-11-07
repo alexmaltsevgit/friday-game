b4029ce79b32748fbd8d5cb069b01f1e428787f5
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-07 18:26:00 +0300

    leave room on 'RoomScreen' cleanup

--- src/components/screens/RoomScreen/RoomScreen.tsx
+++ src/components/screens/RoomScreen/RoomScreen.tsx
@@ -1,4 +1,6 @@
-import { useRoomState } from "@/service/store";
+import { onCleanup } from "solid-js";
+
+import { useLeaveRoom, useRoomState } from "@/service/store";
 import { RoomStatus } from "@/types";
 import { AwaitingScreen } from "./components";
 
@@ -8,7 +10,11 @@ const statusToScreenMap = {
 
 export const RoomScreen = () => {
   const roomState = useRoomState();
+  const leaveRoomMutation = useLeaveRoom();
+
   const status = roomState.game.status;
+
+  onCleanup(leaveRoomMutation.mutate);
 
   return <>{statusToScreenMap[status] ?? <div>Error</div>}</>;
 };
