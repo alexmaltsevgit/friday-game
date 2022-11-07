339c83f921dc8cc4283af981f7368fb9d2452a86
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-07 18:41:39 +0300

    finish JoinScreen

--- src/components/screens/JoinScreen/JoinScreen.tsx
+++ src/components/screens/JoinScreen/JoinScreen.tsx
@@ -4,6 +4,7 @@ import { createSignal } from "solid-js";
 import { model as modelDir } from "@/directives";
 import { Button } from "@/components/shared";
 import { routes } from "@/service/routes";
+import { useJoinRoom } from "@/service/store";
 
 import styles from "./JoinScreen.module.scss";
 
@@ -17,19 +18,33 @@ export const JoinScreen = () => {
   const navigate = useNavigate();
   const { id } = useParams<Params>();
 
+  const [name, setName] = createSignal<string>();
   const [code, setCode] = createSignal(id);
+
+  const mutation = useJoinRoom();
+  const onJoinRoom = () => mutation.mutate({ realName: name(), code: code() });
 
   const onBack = () => navigate(routes.index());
 
   return (
     <div class={styles.root}>
       <div class={styles.content}>
-        <h2>Введите код игры</h2>
+        <h2>Введите свое имя и код игры</h2>
 
-        <input use:model={[code, setCode]} class={styles.input} />
+        <div class={styles.form}>
+          <div class={styles.inputContainer}>
+            <div class={styles.inputLabel}>Ваше имя</div>
+            <input use:model={[name, setName]} class={styles.input} />
+          </div>
+
+          <div class={styles.inputContainer}>
+            <div class={styles.inputLabel}>Код игры</div>
+            <input use:model={[code, setCode]} class={styles.input} />
+          </div>
+        </div>
 
         <div class={styles.controls}>
-          <Button>Присоединиться</Button>
+          <Button onClick={onJoinRoom}>Присоединиться</Button>
 
           <Button variant="outlined" onClick={onBack}>
             Назад
