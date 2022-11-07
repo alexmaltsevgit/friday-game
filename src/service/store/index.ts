54460654acea06d5a50b5ec774eb89d205b858b4
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-07 18:24:22 +0300

    refactor store & add 'useLeaveRoom'

--- src/service/store/index.ts
+++ src/service/store/index.ts
@@ -1,6 +1,7 @@
-import { Client } from "colyseus.js";
+import { Client, Room } from "colyseus.js";
 import { createStore } from "solid-js/store";
 import { createMutation } from "@tanstack/solid-query";
+import { batch, createSignal } from "solid-js";
 
 import {
   GameColyseusState,
@@ -11,22 +12,41 @@ import { mapColyseusStateToPlainState } from "@/utils";
 
 const socket = new Client(import.meta.env.VITE_WS_SERVER_URL_DEV);
 
+const initialStore: PlainStateFromColyseusState<RoomColyseusState> = {};
+
+const [room, setRoom] = createSignal<Room>();
+
 const [roomStore, setRoomStore] =
-  createStore<PlainStateFromColyseusState<RoomColyseusState> | null>(null);
+  createStore<PlainStateFromColyseusState<RoomColyseusState>>(initialStore);
+
+const clearStore = () =>
+  setRoomStore({
+    roomId: undefined,
+    game: undefined,
+    myId: undefined,
+  });
+
+const createStateChangeListener =
+  (room: Room<GameColyseusState>) => (state: GameColyseusState) => {
+    batch(() => {
+      // you can move this few lines out of state listener, but you should not
+      setRoomStore("roomId", room.id);
+      setRoomStore("myId", room.sessionId);
+
+      setRoomStore("game", mapColyseusStateToPlainState(state));
+    });
+  };
+
+const initRoom = (room: Room<GameColyseusState>) => {
+  room.onLeave(clearStore);
+  room.onStateChange(createStateChangeListener(room));
+  setRoom(room);
+};
 
 export const useCreateRoom = () => {
   const mutationFn = async (realName: string) => {
     const room = await socket.create<GameColyseusState>("game", { realName });
-
-    room.onStateChange((s) => {
-      // don't move this few lines out of 'onStateChange'
-      setRoomStore("roomId", room.id);
-      setRoomStore("myId", room.sessionId);
-
-      setRoomStore("game", mapColyseusStateToPlainState(s));
-    });
-
-    return room;
+    initRoom(room);
   };
 
   return createMutation(mutationFn);
@@ -40,18 +60,14 @@ type JoinRoomPayload = {
 export const useJoinRoom = () => {
   const mutationFn = async ({ realName, code }: JoinRoomPayload) => {
     const room = await socket.joinById<GameColyseusState>(code, { realName });
-
-    room.onStateChange((s) => {
-      // don't move this few lines out of 'onStateChange'
-      setRoomStore("roomId", room.id);
-      setRoomStore("myId", room.sessionId);
-
-      setRoomStore("game", mapColyseusStateToPlainState(s));
-    });
-
-    return room;
+    initRoom(room);
   };
 
+  return createMutation(mutationFn);
+};
+
+export const useLeaveRoom = () => {
+  const mutationFn = () => room()?.leave();
   return createMutation(mutationFn);
 };
 
