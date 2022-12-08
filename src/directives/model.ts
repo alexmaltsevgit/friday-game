3e3e0a865b9c06d43cba7788b0234bca0d3e66ea
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-12-08 22:00:40 +0300

    better typing for 'use:model'

--- src/directives/model.ts
+++ src/directives/model.ts
@@ -1,7 +1,17 @@
 import { createRenderEffect } from "solid-js";
 
+export type ModelValue = [() => string, (v: string) => void];
+
+declare module "solid-js" {
+  namespace JSX {
+    interface Directives {
+      model: ModelValue;
+    }
+  }
+}
+
 // https://www.solidjs.com/docs/latest/api#use___
-export const model = (el, value) => {
+export const model = (el, value: () => ModelValue) => {
   if (!value) return;
 
   const [field, setField] = value();
