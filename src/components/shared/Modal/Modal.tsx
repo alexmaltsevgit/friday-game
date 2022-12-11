1e9e4ebe93d4c628c996bf3f4ae5bb60994fa0a7
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-12-11 23:12:35 +0300

    fix Modal conditional rendering

--- src/components/shared/Modal/Modal.tsx
+++ src/components/shared/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { Component, createEffect, onMount, Show, onCleanup } from "solid-js";
+import { Component, onMount, Show, onCleanup } from "solid-js";
 
 import { closeModal, useModalStore } from "@/service/store";
 
@@ -6,23 +6,32 @@ import styles from "./Modal.module.scss";
 
 export const Modal: Component = () => {
   const modalStore = useModalStore();
+  return (
+    <Show when={modalStore.isOpen}>
+      <ModalContent />
+    </Show>
+  );
+};
+
+const ModalContent: Component = () => {
+  const modalStore = useModalStore();
 
   let modalRef: HTMLDivElement;
 
   const onDocumentClick = (e: MouseEvent) => {
-    if (!modalRef?.contains(e.target as Node)) closeModal();
+    if (!modalRef?.contains(e.target as Node)) {
+      closeModal();
+    }
   };
 
   onMount(() => document.addEventListener("click", onDocumentClick));
   onCleanup(() => document.removeEventListener("click", onDocumentClick));
 
   return (
-    <Show when={modalStore.isOpen}>
-      <div class={styles.root}>
-        <div class={styles.modal} ref={modalRef}>
-          {modalStore.component}
-        </div>
+    <div class={styles.root}>
+      <div class={styles.modal} ref={modalRef}>
+        {modalStore.component}
       </div>
-    </Show>
+    </div>
   );
 };
